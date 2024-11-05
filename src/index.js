93939b10b05acc144d47bc68e526feef61506f15
h-0y28 <s24015@gsm.hs.kr>
2024-11-05 21:15:41 +0900

    store and reducer

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
+import { legacy_createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+// reduceer이나 modifier은 처음으로 data를 바꿔줌
+// state = 0 => state가 undefind일 때 0으로 초기화 해줌
+const countmodifier = (count = 0) => {
+  return count;
 };
 
-const handleAdd = () => {
-  // console.log("add");
-  count++;
-  updateText();
-};
+const countStore = legacy_createStore(countmodifier);
 
-const handleMinus = () => {
-  count--;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore);
