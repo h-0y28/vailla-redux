bdbd8b40fe6ee0b86a72de4f93f73061c867d5ad
h-0y28 <s24015@gsm.hs.kr>
2024-11-06 15:17:12 +0900

    action

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-// reduceer이나 modifier은 처음으로 data를 바꿔줌
-// state = 0 => state가 undefind일 때 0으로 초기화 해줌
-const countmodifier = (count = 0) => {
-  return count;
+// action: 전송받은 message저장
+const countmodifier = (count = 0, action) => {
+  console.log(action); // 처음으로i intialized된 function출력
+  if (action.type === "ADD") return count + 1;
+  else if (action.type === "MINUS") return count - 1;
+  else return count;
 };
 
 const countStore = legacy_createStore(countmodifier);
 
-console.log(countStore);
+countStore.dispatch({ type: "ADD" }); // dispatch: message를 전송하는 방법
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
+console.log(countStore.getState());
