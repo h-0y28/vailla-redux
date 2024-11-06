4f627ecae045c5bde762b636458a129055b81d8f
h-0y28 <s24015@gsm.hs.kr>
2024-11-06 17:07:49 +0900

    recap refactor

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
+// 휴먼에러를 방지하기 위함 (오타)
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+// 현재 상태의 application과 함께 불려지는 함수
+// return하는 것은 apllication의 state가 됨
 const countmodifier = (count = 0, action) => {
-  if (action.type === "ADD") return count + 1;
-  else if (action.type === "MINUS") return count - 1;
-  else return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
 
 const countStore = legacy_createStore(countmodifier);
@@ -18,14 +31,14 @@ const onChange = () => {
   number.innerText = countStore.getState();
 };
 
-countStore.subscribe(onChange);
+countStore.subscribe(onChange); // store에서 change를 감지하고 싶을 때
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD }); // action은 objedt이어야함 (sting 금지) , 무조건 type이라고 해야함 (name금지)
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
